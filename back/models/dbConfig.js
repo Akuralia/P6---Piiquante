770a2490ec2ba4c29b3a75e8f7540b1f35185d47
Akuralia <warahunter@gmail.com>
2023-01-05 10:56:03 +0100

    [ADD] User Controller [ADD] User Routes [ADD] User Model [ADD] User Middleware

--- back/models/dbConfig.js
+++ back/models/dbConfig.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    "mongodb://localhost:27017/piquante-api",
-    { useNewUrlParser : true, useUnifiedTopology: true},
-    (err) => {
-        if(!err)
-    }
-)
+mongoose.connect('mongodb+srv://akuralia:CQc3Pv6QWoqxidcB@cluster0.cycfg08.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
